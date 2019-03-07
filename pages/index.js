3661c2acb197d9d2b64ef07ce76ff0c3a241749f
汤林 <tanglin@topshifts.com>
2019-03-07 17:29:15 +0800

    stock

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,19 @@ export default class Index extends React.Component {
               <div className="flex-container">
                 <div className="flex-item">{show.id}</div>
                 <div className="flex-item">{show.name}</div>
-                <div className="flex-item">{show.currentPrice}</div>
-                <div className="flex-item">{show.upperPercent}</div>
+                <div className="flex-item">{show.currentPrice}万</div>
+                <div className={show.upperPercent > 0 ? "flex-item text-red": show.upperPercent == 0 ? "flex-item" : "flex-item text-green" }>{show.upperPercent}%</div>
                 <div className="flex-item">{show.changeAmount}</div>
                 <div className="flex-item">{show.buy}</div>
                 <div className="flex-item">{show.sell}</div>
                 <div className="flex-item">{show.AcurrentPrice}</div>
-                <div className="flex-item">{show.AUpperPercent}</div>
+                <div className="flex-item">{show.ABuy}</div>
+                <div className="flex-item">{show.ASell}</div>
+                <div className="flex-item">{show.AChangeAmount}万</div>
+                <div className="flex-item">{show.AUpperPercent}%</div>
                 <div className="flex-item">{show.yesterdayPrice}</div>
-                <div className="flex-item">{show.zhishuUpperPrice}</div>
-                <div className="flex-item">{show.zheyiPrice}</div>
+                <div className={show.zhishuUpperPrice > 0 ? "flex-item text-red": show.zhishuUpperPrice == 0 ? "flex-item" : "flex-item text-green" }>{show.zhishuUpperPrice}</div>
+                <div className={show.zheyiPrice > 0 ? "flex-item text-red": show.zheyiPrice == 0 ? "flex-item" : "flex-item text-green" }>{show.zheyiPrice}{show.zheyiPrice ? "%" : ""}</div>
                 <div className="flex-item">{show.abPercent}</div>
               </div>
             </li>
@@ -62,6 +65,12 @@ export default class Index extends React.Component {
           .flex-item {
             flex:1
           }
+          .text-red {
+            color:red;
+          }
+          .text-green {
+            color:green;
+          }
 
         `}</style>
       </Layout>)
@@ -90,28 +99,6 @@ async function getUser() {
   const res = await instance.get('/Data/Fund_JJJZ_Data.aspx?t=1&lx=9&letter=&gsid=0&text=&sort=zdf,desc&page=1,10000000&dt=1425897162484&atfc=');
   return res.data;
 }
-const match = { '162412' : {code:'sz399989', a:5},
-                '502056' : 'sz399989',
-                '160420' : 'sz399673',
-                '160219' : 'sz399913',
-                '161726' : 'sz399441',
-                "163118" : "sz399441",
-                "160127" : "sz399932",
-                "165519" : "sh000841",
-                "161022" : "sz399006",
-                "161725" : "sz399997",
-                "163209" : "sz399958",
-                "160636" : "sz399675",
-                "160637" : "sz399006",
-                "161025" : "sz399675",
-                "168001" : "sz399812",
-                "161223" : "sz399006",
-                "160634" : "sh000827",
-                "164819" : "sh000827",
-                "162413" : "sh000852",
-                "162216" : "sz399905",
-                "" : ""}
-
  const codes = {
                 '150262'  : { a : '150261', z : '162412', ab : 5, code : 'sz399989'},
                 '502058'  : { a : '502057', z : '502056', ab : 5, code : 'sz399989'},
@@ -203,19 +190,18 @@ const match = { '162412' : {code:'sz399989', a:5},
                 '150222'  : { a : '150221', z : '164402', ab : 5, code : 'sz399959'},
                 '150252'  : { a : '150251', z : '161724', ab : 5, code : 'sz399990'},
                 '150174'  : { a : '150173', z : '165522', ab : 5, code : 'sh000998'},
-                '150176'  : { a : '150175', z : '161831', ab : 5, code : ''},
-                '150290'  : { a : '150289', z : '168204', ab : 5, code : ''},
-                '502038'  : { a : '502037', z : '502036', ab : 5, code : ''},
-                '150318'  : { a : '150317', z : '164907', ab : 5, code : ''},
-                '150282'  : { a : '150281', z : '160814', ab : 5, code : ''},
-                '150158'  : { a : '150157', z : '165521', ab : 5, code : ''},
-                '150330'  : { a : '150329', z : '167301', ab : 5, code : ''},
-                '150226'  : { a : '150225', z : '160516', ab : 5, code : ''},
-                '150344'  : { a : '150343', z : '161629', ab : 5, code : ''},
-                '502055'  : { a : '502054', z : '502053', ab : 5, code : ''},
-                '150172'  : { a : '150171', z : '163113', ab : 5, code : ''},
-                '502012'  : { a : '502011', z : '502010', ab : 5, code : ''},
-                '150236'  : { a : '150235', z : '160633', ab : 5, code : ''}
+                '150290'  : { a : '150289', z : '168204', ab : 5, code : 'sz399998'},
+                '502038'  : { a : '502037', z : '502036', ab : 5, code : 'sz399805'},
+                '150318'  : { a : '150317', z : '164907', ab : 5, code : 'sz399805'},
+                '150282'  : { a : '150281', z : '160814', ab : 5, code : 'sz399934'},
+                '150158'  : { a : '150157', z : '165521', ab : 5, code : 'sh000974'},
+                '150330'  : { a : '150329', z : '167301', ab : 5, code : 'sz399809'},
+                '150226'  : { a : '150225', z : '160516', ab : 5, code : 'sz399966'},
+                '150344'  : { a : '150343', z : '161629', ab : 5, code : 'sz399975'},
+                '502055'  : { a : '502054', z : '502053', ab : 5, code : 'sz399975'},
+                '150172'  : { a : '150171', z : '163113', ab : 5, code : 'sz399707'},
+                '502012'  : { a : '502011', z : '502010', ab : 5, code : 'sz399975'},
+                '150236'  : { a : '150235', z : '160633', ab : 5, code : 'sz399975'}
               }
 Index.getInitialProps = async function() {
   // const res = await fetch('http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=9&letter=&gsid=0&text=&sort=zdf,desc&page=1,10000000&dt=1425897162484&atfc=')
@@ -231,46 +217,116 @@ Index.getInitialProps = async function() {
     return json; 
   } 
   
-  const c = strToJson(d);
-  const ret = []
-  ret.push({id:"代码", name:"名称", currentPrice:"现价", buy:"买一", sell:"卖一", changeAmount:"成交金额", upperPercent:"涨幅", AcurrentPrice:"A类现价",AUpperPercent:"A类涨幅",  culPrice:"母鸡估值",yesterdayPrice:"昨天净值",zhishuUpperPrice:"指数涨幅", zheyiPrice:"折溢价",abPercent:"ab份额比"})
-  c['datas'].map((show) => {
-    const dic = {};
-    dic.id = show[0];   
-    dic.name = show[1];
-    dic.yesterdayPrice = show[3];
-    ret.push(dic);  
-  });
-  const group = new Map();
-  const stocks = new Map();
-  ret.map((item) => {
-    let eachgroup = [];
-    let key = "";
-    let target = item.name.substring(0,item.name.length);
+  const mujiDic = {}
+  const ADic = {}
+  const zhishuDic = {}
 
-    ret.map((item)=> {
-      stocks.set(item.id, item);
-      let matched = true;
-      target.split("").map((char) => {
-        if (item.name.indexOf(char) === -1) {
-          matched = false;
-        }
-      });
-      if (matched) {
-        eachgroup.push(item.id);
-        eachgroup = eachgroup.sort((x,y) => x-y)
-        key = eachgroup[0];
-      } 
-    })
-    if (eachgroup.length > 1) {
-      group.set(key, {group:eachgroup, item: stocks.get(key)})
-      eachgroup.map((each) => {
-        realStock.push("sz" + each);
-        realStock.push("sh" + each);
-      })
+  for (let code in codes) {
+    mujiDic[codes[code].z] = code;
+    ADic[codes[code].a] = code;
+    if (zhishuDic[codes[code].code] === undefined) {
+      zhishuDic[codes[code].code] = [];
     }
-  })
+    zhishuDic[codes[code].code].push(code);
+    realStock.push(parseInt(code) > 200000 ? 'sh' + code : 'sz' + code);
+    realStock.push(parseInt(codes[code].a) > 200000 ? 'sh' + codes[code].a : 'sz' + codes[code].a);
+    realStock.push(codes[code].code);
+  }
 
+  const c = strToJson(d);
+  const ret = {}
+  ret[0] = ({id:"代码", name:"名称", currentPrice:"现价", buy:"买一", sell:"卖一", changeAmount:"成交金额", upperPercent:"涨幅", AcurrentPrice:"A类现价", ABuy:"A类买一", ASell:"A类卖一", AChangeAmount:"A类交易额", AUpperPercent:"A类涨幅",  culPrice:"母鸡估值",yesterdayPrice:"昨天净值",zhishuUpperPrice:"指数涨幅", zheyiPrice:"折溢价",abPercent:"ab份额比"})
+  c['datas'].map((show) => {
+    if (show[0] in codes) {
+      const dic = {};
+      dic.id = show[0];   
+      ret[show[0]] = dic 
+    }
+  });
+
+  c['datas'].map((show) => {
+    if (show[0] in mujiDic) {
+      let yes = show[3];
+      if (show[3] === "") {
+        if (global && global.stock) {
+          yes = global.stock[show[0]]
+        }
+      } else {
+        if (global) {
+          if (global.stock === undefined) {
+            global.stock = {}
+          }
+          global.stock[show[0]] = yes;
+        }
+      }
+
+      ret[mujiDic[show[0]]].yesterdayPrice = yes;
+      ret[mujiDic[show[0]]].name = show[1];
+    }
+  });
+
+
+ 
+
+  const stockPrice = await fetch("http://hq.sinajs.cn/list=" + realStock.join(","))
+  const priceRes = await stockPrice.text();
+
+
+  // console.log('sina -->', priceRes);
+
+  for (let code in codes) {
+    let key = parseInt(code) > 200000 ? 'sh' + code : 'sz' + code;
+    let regex = new RegExp(key + "=\"(.*?)\";");
+    let match = regex.exec(priceRes);
+    let stockInfoArray = match[1].split(",")
+    let yesterday = stockInfoArray[2];
+    ret[code].currentPrice = stockInfoArray[3] < 0.00001 ? stockInfoArray[2] : stockInfoArray[3];
+    ret[code].buy = stockInfoArray[11]
+    ret[code].sell = stockInfoArray[21]
+    ret[code].changeAmount = parseFloat(parseInt(stockInfoArray[9] / 100) * 100 / 10000)
+    ret[code].upperPercent = parseInt((ret[code].currentPrice - yesterday)/yesterday  * 10000) / 100 
+    ret[code].abPercent = codes[code].ab + ":" + (10 - codes[code].ab )
+
+  }
+
+  for (let code in ADic) {
+    let key = parseInt(code) > 200000 ? 'sh' + code : 'sz' + code;
+    let regex = new RegExp(key + "=\"(.*?)\";");
+    let match = regex.exec(priceRes);
+    let stockInfoArray = match[1].split(",");
+
+    ret[ADic[code]].AcurrentPrice = stockInfoArray[3] < 0.00001 ? stockInfoArray[2] : stockInfoArray[3];
+    ret[ADic[code]].ABuy = stockInfoArray[11]
+    ret[ADic[code]].ASell = stockInfoArray[21]
+    ret[ADic[code]].AChangeAmount = parseFloat(parseInt(stockInfoArray[9] / 100) * 100 / 10000)
+    ret[ADic[code]].AUpperPercent = parseInt((stockInfoArray[3]- stockInfoArray[2])/stockInfoArray[3] * 10000) / 100 
+    console.log('current', stockInfoArray[3])
+    console.log('yesterday', stockInfoArray[2])
+    console.log('code', ADic[code])
+  }
+
+  for (let code in zhishuDic) {
+    let regex = new RegExp(code + "=\"(.*?)\";");
+    let match = regex.exec(priceRes);
+    let stockInfoArray = match[1].split(",");
+
+    zhishuDic[code].map( bCode => {
+      ret[bCode].zhishuUpperPrice = parseInt((stockInfoArray[3]- stockInfoArray[2])/stockInfoArray[3] * 10000) / 100 
+      let jingzhi = ret[bCode].yesterdayPrice * (stockInfoArray[3] / stockInfoArray[2] * 0.995);
+  
+      
+      let price = (ret[bCode].AcurrentPrice * codes[bCode].ab + ret[bCode].currentPrice * (10 - codes[bCode].ab)) / 10
+      ret[bCode].zheyiPrice = parseInt((price - jingzhi)/ jingzhi * 100) / 100
+      ret[bCode].culPrice = jingzhi;
+    });
+
+
+  }
+
+  let stocks = []
+  for(let code in ret) {
+    stocks.push(ret[code])
+  }
 
   function _strMapToObj(strMap){
     let obj= Object.create(null);
@@ -280,35 +336,11 @@ Index.getInitialProps = async function() {
     return obj;
   }
 
-  const stockPrice = await fetch("http://hq.sinajs.cn/list=" + realStock.join(","))
-  const priceRes = await stockPrice.text();
-
-  for (let key of group.keys()) {
-
-    let codes = group.get(key).group;
-    let item = group.get(key).item;
-    
-    if (codes.length === 3) {
-      codes.map(code => {
-        let regex = new RegExp(key + "=\"(.*?)\";");
-        let match = regex.exec(priceRes);
-        let stockInfoArray = match[1].split(",")
-        item.todayPrice = stockInfoArray[3]
-        if(!item.todayPrice) {
-          console.log("母鸡或债基",code,group.get(code))
-        }
-
-      })
-
-      
-    }
-  }
-
-
-
+  // stocks = stocks.sort((x,y) => x.zheyiPrice-y.zheyiPrice);
+  stocks = stocks.sort((x,y) => y.changeAmount-x.changeAmount);
   return {
-    shows: ret,
-    group:_strMapToObj(group),
+    shows: stocks,
+    // group:_strMapToObj(group), 
     realStock:realStock,
     param:priceRes
   }
